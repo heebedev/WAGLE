6c5e170f85710240d024d68061b2851e9ced930b
Prayha <jhj9512z@naver.com>
2020-07-24 19:29:11 +0900

    마이페이지 독후감까지 완료

--- app/src/main/java/com/androidlec/wagle/jhj/Jhj_MyPage_Wagle_List_Adapter.java
+++ app/src/main/java/com/androidlec/wagle/jhj/Jhj_MyPage_Wagle_List_Adapter.java
@@ -8,15 +8,15 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.androidlec.wagle.CS.Model.WagleList;
 import com.androidlec.wagle.R;
 
 import java.util.ArrayList;
 
-public class Jhj_Post_DHG_List_Adapter extends RecyclerView.Adapter<Jhj_Post_DHG_List_Adapter.ViewHolder> {
-
+public class Jhj_MyPage_Wagle_List_Adapter extends RecyclerView.Adapter<Jhj_MyPage_Wagle_List_Adapter.ViewHolder> {
 
     Context context;
-    private ArrayList<Jhj_BookReport_DTO> data;
+    private ArrayList<WagleList> data;
 
     // Activity에서 클릭이벤트를 사용하기위한 준비
     public interface OnItemClickListener {
@@ -27,14 +27,14 @@ public class Jhj_Post_DHG_List_Adapter extends RecyclerView.Adapter<Jhj_Post_DHG
         void onItemLongClickListener(View v, int position);
     }
 
-    private Jhj_Post_DHG_List_Adapter.OnItemClickListener mListener = null;
-    private Jhj_Post_DHG_List_Adapter.OnItemLongClickListener mLongListener = null;
+    private Jhj_MyPage_Wagle_List_Adapter.OnItemClickListener mListener = null;
+    private Jhj_MyPage_Wagle_List_Adapter.OnItemLongClickListener mLongListener = null;
 
-    public void setOnItemClickListener(Jhj_Post_DHG_List_Adapter.OnItemClickListener listener) {
+    public void setOnItemClickListener(Jhj_MyPage_Wagle_List_Adapter.OnItemClickListener listener) {
         this.mListener = listener;
     }
 
-    public void setOnItemLongClickListener(Jhj_Post_DHG_List_Adapter.OnItemLongClickListener listener) {
+    public void setOnItemLongClickListener(Jhj_MyPage_Wagle_List_Adapter.OnItemLongClickListener listener) {
         this.mLongListener = listener;
     }
 
@@ -80,26 +80,26 @@ public class Jhj_Post_DHG_List_Adapter extends RecyclerView.Adapter<Jhj_Post_DHG
     }
 
     // 생성자에서 데이터 리스트 객체를 전달받음.
-    Jhj_Post_DHG_List_Adapter(ArrayList<Jhj_BookReport_DTO> list) {
+    Jhj_MyPage_Wagle_List_Adapter(ArrayList<WagleList> list) {
         data = list ;
     }
 
     // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴.
     @Override
-    public Jhj_Post_DHG_List_Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public Jhj_MyPage_Wagle_List_Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         context = parent.getContext() ;
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
 
         View view = inflater.inflate(R.layout.jhj_post_notice_list_item, parent, false) ;
-        Jhj_Post_DHG_List_Adapter.ViewHolder vh = new Jhj_Post_DHG_List_Adapter.ViewHolder(view) ;
+        Jhj_MyPage_Wagle_List_Adapter.ViewHolder vh = new Jhj_MyPage_Wagle_List_Adapter.ViewHolder(view) ;
 
         return vh ;
     }
 
     // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
     @Override
-    public void onBindViewHolder(Jhj_Post_DHG_List_Adapter.ViewHolder holder, int position) {
-        String ListItem = data.get(position).getWcName() + " - " + data.get(position).getuName();
+    public void onBindViewHolder(Jhj_MyPage_Wagle_List_Adapter.ViewHolder holder, int position) {
+        String ListItem = data.get(position).getWcName();
         holder.textView1.setText(ListItem) ;
     }
 
