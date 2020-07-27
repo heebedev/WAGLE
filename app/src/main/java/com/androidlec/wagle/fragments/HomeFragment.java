34bf0a276b421f37195019b1e5288e14883abcd8
TJ <tj@TJui-iMac-10.local>
2020-07-27 11:34:01 +0900

    0727 restore Friday

--- app/src/main/java/com/androidlec/wagle/fragments/HomeFragment.java
+++ app/src/main/java/com/androidlec/wagle/fragments/HomeFragment.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +17,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.androidlec.wagle.BoardFragment;
 import com.androidlec.wagle.CS.Model.WagleList;
 import com.androidlec.wagle.HomeActivity;
 import com.androidlec.wagle.JH.MyWagleActivity;
@@ -46,22 +49,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     // 지워야할것.
     private static String seqno = Integer.toString(UserInfo.USEQNO);
@@ -83,34 +71,6 @@ public class HomeFragment extends Fragment {
 
     public HomeFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-
-        }
     }
 
     @Override
@@ -155,6 +115,13 @@ public class HomeFragment extends Fragment {
         Gallery_Setting();
         // 독후감 세팅
         BookReport_Setting();
+
+        // 초기 Fragment
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        BoardFragment boardFragment = new BoardFragment();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_home_boardContainer, boardFragment).commitAllowingStateLoss();
+
     }
 
     protected String Post_Select_All(String urlAddr) {
