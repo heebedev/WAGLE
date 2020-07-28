fd417e4de61701639a19e6ec565b27497e7d436b
jonghak <67566238+jonghak96@users.noreply.github.com>
2020-07-28 17:40:10 +0900

    회원가입할때 아이디로 이미지 저장.

--- app/src/main/java/com/androidlec/wagle/networkTask/JH_ConnectFTP.java
+++ app/src/main/java/com/androidlec/wagle/networkTask/JH_ConnectFTP.java
@@ -24,14 +24,16 @@ public class JH_ConnectFTP extends AsyncTask<Integer, String, String> {
     String password;
     int port;
     Uri file;
+    String uId;
 
-    public JH_ConnectFTP(Context context, String host, String username, String password, int port, Uri file) {
+    public JH_ConnectFTP(Context context, String host, String username, String password, int port, Uri file, String uId) {
         this.context = context;
         this.host = host;
         this.username = username;
         this.password = password;
         this.port = port;
         this.file = file;
+        this.uId = uId;
         mFTPClient = new FTPClient();
     }
 
@@ -45,17 +47,8 @@ public class JH_ConnectFTP extends AsyncTask<Integer, String, String> {
         if (status = ftpConnect(host, username, password, port)) {
             String currentPath = ftpGetDirectory() + "userImgs";
 
-            // 현재시간을 msec 으로 구한다.
-            long now = System.currentTimeMillis();
-            // 현재시간을 date 변수에 저장한다.
-            Date date = new Date(now);
-            // 시간을 나타냇 포맷을 정한다 ( yyyy/MM/dd 같은 형태로 변형 가능 )
-            SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-            // nowDate 변수에 값을 저장한다.
-            formatDate = sdfNow.format(date);
-
             // 파일 업로드시
-            if (ftpUploadFile(file, formatDate + ".jpg", currentPath)) {
+            if (ftpUploadFile(file, uId + ".jpg", currentPath)) {
             }
         }
 
@@ -63,7 +56,7 @@ public class JH_ConnectFTP extends AsyncTask<Integer, String, String> {
             ftpDisconnect();
         }
 
-        return formatDate + ".jpg";
+        return uId + ".jpg";
     }
 
     public boolean ftpConnect(String host, String username, String password, int port) {
